6bd4eb7ca95a1750f41aad2c37551d366331e64b
jony <jonybhattcharjee886@gmail.com>
2024-11-20 00:22:50 +0600

    Added inter family as a font

--- App.tsx
+++ App.tsx
@@ -2,7 +2,31 @@ import { StatusBar } from "expo-status-bar";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import DayList from "./src/component/core/DayList";
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_900Black,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
 export default function App() {
+  const [loaded, error] = useFonts({
+    Inter_900Black,
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.head}>Advant Calender</Text>
@@ -22,5 +46,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     padding: 20,
+    fontFamily: " Inter_900Black",
   },
 });
