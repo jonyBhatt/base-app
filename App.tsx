a9aa08afc1280409e5b1c69b9cef348d44b58bad
jony <jonybhattcharjee886@gmail.com>
2024-11-20 00:04:05 +0600

    remove the day list item from the file and clean up the file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import DayList from "./src/component/core/DayList";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.head}>Advant Calender</Text>
+      <DayList />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  head: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 20,
   },
 });
