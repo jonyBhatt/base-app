03aa1a59258c167318fea7e8c6dc6b790b1f7048
jony <jonybhattcharjee886@gmail.com>
2024-11-20 17:36:10 +0600

    Refactor the font to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
 import { Stack } from "expo-router";
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_900Black,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { Eater_400Regular } from "@expo-google-fonts/eater";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Inter_900Black,
+    Inter_400Regular,
+    Inter_500Medium,
+    Eater: Eater_400Regular,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Home" }} />
--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,12 @@
-import {
-  Inter_400Regular,
-  Inter_500Medium,
-  Inter_900Black,
-  useFonts,
-} from "@expo-google-fonts/inter";
-import { Eater_400Regular } from "@expo-google-fonts/eater";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
 import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import DayList from "../src/component/core/DayList";
 
 export default function App() {
-  const [loaded, error] = useFonts({
-    Inter_900Black,
-    Inter_400Regular,
-    Inter_500Medium,
-    Eater: Eater_400Regular,
-  });
-
-  useEffect(() => {
-    if (loaded || error) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded, error]);
-
-  if (!loaded && !error) {
-    return null;
-  }
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.head}>Advant Calender</Text>
       <DayList />
-      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
